d39716eeee5c94ae39537795bb490b36b265b39f
Kerem Akkale <keremakkale@gmail.com>
2026-02-27 20:29:36 +0300

    ilk commit

---
+++ src/main/java/com/smartfarm/smartfarmmanagementsystem/SmartFarmManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smartfarm.smartfarmmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFarmManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFarmManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartfarm/smartfarmmanagementsystem/SmartFarmManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartfarm.smartfarmmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFarmManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
